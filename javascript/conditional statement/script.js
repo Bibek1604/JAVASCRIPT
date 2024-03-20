1433c55414f7110a2ee9500d0ff32b6aca7d58de
Bibek Pandey <145880788+imBibek-Pandey@users.noreply.github.com>
2024-03-20 22:17:45 +0545

    conditional and loops

--- javascript/conditional statement/script.js
+++ javascript/conditional statement/script.js
@@ -1,53 +1,50 @@
-// // // // // // console.log("hello world");
+// // // // // // // // console.log("hello world");
 
 
-// // // // // //operators in js || use to do some expression or operator
+// // // // // // // //operators in js || use to do some expression or operator
 
-// // // // // //addition
+// // // // // // // //addition
 
-// // // // // // a=prompt("enter the First value: ");
-// // // // // // b=prompt("enter the second value: ");
-// // // // // // c=parseInt(a)+parseInt(b);
-// // // // // // console.log("addition is: ",c);
+// // // // // // // // a=prompt("enter the First value: ");
+// // // // // // // // b=prompt("enter the second value: ");
+// // // // // // // // c=parseInt(a)+parseInt(b);
+// // // // // // // // console.log("addition is: ",c);
 
-// // // // // //modulus(reminder)
+// // // // // // // //modulus(reminder)
 
-// // // // // // a=prompt("enter the First value: ");
-// // // // // // b=prompt("enter the second value: ");
-// // // // // // c=a%b,
-// // // // // // console.log("modulus is: ",c)
-
-
-// // // // // //exponential(power)
-
-// // // // // // a=prompt("Enter the first  number to get power: ");
-// // // // // // b=prompt("Enter the second number to get power: ")
-// // // // // // c=a**b;
-// // // // // // console.log("exponential is: ",c);
+// // // // // // // // a=prompt("enter the First value: ");
+// // // // // // // // b=prompt("enter the second value: ");
+// // // // // // // // c=a%b,
+// // // // // // // // console.log("modulus is: ",c)
 
 
+// // // // // // // //exponential(power)
 
-// // // // // // ###############################################################
-// // // // // //                       UNARY OPERATORS
-// // // // // // ###############################################################
-// // // // // ///increment ++
-
-// // // // // // [[[[post increment]]]]
-// // // // // // a=prompt("enter the number : ");
-// // // // // // console.log(a)
-// // // // // // console.log(a++)
-// // // // // // console.log(a)
-
-// // // // // // [[[[pre increment]]]]
-
-// // // // // // a=prompt("enter the number : ");
-// // // // // // console.log(a)
-// // // // // // console.log(++a)
-
-// // // // // /// decrement --
+// // // // // // // // a=prompt("Enter the first  number to get power: ");
+// // // // // // // // b=prompt("Enter the second number to get power: ")
+// // // // // // // // c=a**b;
+// // // // // // // // console.log("exponential is: ",c);
 
 
 
+// // // // // // // // ###############################################################
+// // // // // // // //                       UNARY OPERATORS
+// // // // // // // // ###############################################################
+// // // // // // // ///increment ++
+
+// // // // // // // // [[[[post increment]]]]
+// // // // // // // // a=prompt("enter the number : ");
+// // // // // // // // console.log(a)
+// // // // // // // // console.log(a++)
+// // // // // // // // console.log(a)
+
+// // // // // // // // [[[[pre increment]]]]
+
+// // // // // // // // a=prompt("enter the number : ");
+// // // // // // // // console.log(a)
+// // // // // // // // console.log(++a)
+
+// // // // // // // /// decrement --
 
 
 
@@ -62,196 +59,229 @@
 
 
 
-// // // // // // ###############################################################
-// // // // // //                       ASSIGNMENT OPERATORS
-// // // // // // ###############################################################
-
-
-// // // // // //addition//
-// // // // // // let a =parseInt(prompt("enter the number: "));
-// // // // // // a += parseInt(prompt("enter the number: "));;
-// // // // // // console.log(a)
-
-
-// // // // // //subtraction//
-
-// // // // // // let a =parseInt(prompt("enter the number: "));
-// // // // // // a -= parseInt(prompt("enter the number: "));
-// // // // // // console.log(a);
-
-// // // // // // modulus
-
-// // // // // // let a =parseInt(prompt("enter the number: "));
-// // // // // // a %= 5
-// // // // // // console.log(a)
-
-
-// // // // // //exponetital
-// // // // // // let a =parseInt(prompt("enter the number: "));
-// // // // // // a *= 5;
-// // // // // // console.log(a);
 
 
 
-// // // // // // ###############################################################
-// // // // // // #                      Comperison OPERATORS                   #
-// // // // // // ###############################################################
-
-// // // // // // ##Equal to
-// // // // // // a=parseInt(prompt("enter the first number: "));
-// // // // // // b=parseInt(prompt("enter the second number: "));
-// // // // // // console.log("a == b: ",a==b) ;
+// // // // // // // // ###############################################################
+// // // // // // // //                       ASSIGNMENT OPERATORS
+// // // // // // // // ###############################################################
 
 
-
-// // // // // // ###is not Equal to
-// // // // // // a=parseInt(prompt("Enter the first number: "));
-// // // // // // b=parseInt(prompt("Enter the second number: "));
-// // // // // // console.log(a != b);
-
-
-// // // // // // ###############################################################
-// // // // // // #                      Comperison OPERATORS                   #
-// // // // // // ###############################################################
+// // // // // // // //addition//
+// // // // // // // // let a =parseInt(prompt("enter the number: "));
+// // // // // // // // a += parseInt(prompt("enter the number: "));;
+// // // // // // // // console.log(a)
 
 
+// // // // // // // //subtraction//
 
-// // // // // // Logical AND &&
+// // // // // // // // let a =parseInt(prompt("enter the number: "));
+// // // // // // // // a -= parseInt(prompt("enter the number: "));
+// // // // // // // // console.log(a);
 
-// // // // // // a=parseInt(prompt("Enter the first number: "));
-// // // // // // b=parseInt(prompt("Enter the second number: "));
-// // // // // // c= a>b;
-// // // // // // d= a===b;
-// // // // // // console.log(c && d);
+// // // // // // // // modulus
+
+// // // // // // // // let a =parseInt(prompt("enter the number: "));
+// // // // // // // // a %= 5
+// // // // // // // // console.log(a)
+
+
+// // // // // // // //exponetital
+// // // // // // // // let a =parseInt(prompt("enter the number: "));
+// // // // // // // // a *= 5;
+// // // // // // // // console.log(a);
 
 
 
-// // // // // // Logical Or ||
-// // // // // // a=parseInt(prompt("Enter the first number: "));
-// // // // // // b=parseInt(prompt("Enter the second number: ")); 
-// // // // // // c= a>b;
-// // // // // // console.log(c)
+// // // // // // // // // ###############################################################
+// // // // // // // // // #                      Comperison OPERATORS                   #
+// // // // // // // // // ###############################################################
+
+// // // // // // // // // ##Equal to
+// // // // // // // // // a=parseInt(prompt("enter the first number: "));
+// // // // // // // // // b=parseInt(prompt("enter the second number: "));
+// // // // // // // // // console.log("a == b: ",a==b) ;
 
 
-// // // // // //###############################################################
-// // // // // // #                           CONDITIONAL STATEMENT                 #
-// // // // // // ###############################################################
 
-// // // // // // let a =parseInt(prompt("Enter the number: "))
-// // // // // // if (a>18){
-// // // // // //     console.log('you can vote!')
+// // // // // // // // // ###is not Equal to
+// // // // // // // // // a=parseInt(prompt("Enter the first number: "));
+// // // // // // // // // b=parseInt(prompt("Enter the second number: "));
+// // // // // // // // // console.log(a != b);
+
+
+// // // // // // // // // ###############################################################
+// // // // // // // // // #                      Comperison OPERATORS                   #
+// // // // // // // // // ###############################################################
+
+
+
+// // // // // // // // // Logical AND &&
+
+// // // // // // // // // a=parseInt(prompt("Enter the first number: "));
+// // // // // // // // // b=parseInt(prompt("Enter the second number: "));
+// // // // // // // // // c= a>b;
+// // // // // // // // // d= a===b;
+// // // // // // // // // console.log(c && d);
+
+
+
+// // // // // // // // // Logical Or ||
+// // // // // // // // // a=parseInt(prompt("Enter the first number: "));
+// // // // // // // // // b=parseInt(prompt("Enter the second number: ")); 
+// // // // // // // // // c= a>b;
+// // // // // // // // // console.log(c)
+
+
+// // // // // // // // //###############################################################
+// // // // // // // // // #                           CONDITIONAL STATEMENT                 #
+// // // // // // // // // ###############################################################
+
+// // // // // // // // // let a =parseInt(prompt("Enter the number: "))
+// // // // // // // // // if (a>18){
+// // // // // // // // //     console.log('you can vote!')
+// // // // // // // // // }
+// // // // // // // // // else {
+// // // // // // // // //     console.log("you can't vote!")
+// // // // // // // // // }
+
+
+// // // // // // // // // let mode = prompt("Enter the color: ")
+// // // // // // // // // let color;
+
+// // // // // // // // // if(mode  === 'dark'){
+// // // // // // // // //     color = "black";
+// // // // // // // // // }
+// // // // // // // // // if (mode === 'light'){
+// // // // // // // // //     color="white";
+// // // // // // // // // }
+// // // // // // // // // console.log(color);
+
+
+// // // // // // // // // ##conditional senten ce to print number even or odd
+// // // // // // // // // let a=parseInt(prompt("Enter the number : "));
+// // // // // // // // // let number;
+// // // // // // // // // if (a % 2 == 0){
+// // // // // // // // //     number = "Even";
+// // // // // // // // // }
+// // // // // // // // // else{
+// // // // // // // // //   number = "odd";
+// // // // // // // // // }
+// // // // // // // // // console.log(number)
+
+// // // // // // // // // ####qstn3
+// // // // // // // // // let mode=prompt("Enter the color: ");
+// // // // // // // // // let color;
+// // // // // // // // // if(mode === "dark"){
+// // // // // // // // //     color="black";
+// // // // // // // // // }
+// // // // // // // // // else if (mode === "blue")
+// // // // // // // // // {
+// // // // // // // // //     color="blue";
+// // // // // // // // // }
+// // // // // // // // // else if(mode === "pink")
+// // // // // // // // // {
+// // // // // // // // //     color="pink";
+// // // // // // // // // }
+// // // // // // // // // else{
+// // // // // // // // //     color="white";
+// // // // // // // // // }
+// // // // // // // // // console.log(color);
+
+
+// // // // // // // // /////
+// // // // // // // // let a=parseInt(prompt("enter the number: "));
+// // // // // // // // let number;
+// // // // // // // // if (a  >= 80){
+// // // // // // // //     number = "It is A";
+// // // // // // // // }
+// // // // // // // // else if (a >= 70){
+// // // // // // // //   number = "It is B";
+// // // // // // // // }
+// // // // // // // // else if (a >= 60){
+// // // // // // // //     number = "It is C";
+// // // // // // // // }
+// // // // // // // // else if (a >= 50){
+// // // // // // // //     number = "It is D";
+// // // // // // // // }
+// // // // // // // // else{
+// // // // // // // //     number="it is F";
+// // // // // // // // }
+
+// // // // // // // // console.log(number)
+
+
+// // // // // // // let a=prompt(parseInt("enter the number: "));
+// // // // // // // let b=prompt(parseInt("enter the second number: "));
+// // // // // // // if (a==b){
+// // // // // // //     console.log("1")
+// // // // // // // }
+// // // // // // // else if (a>b){
+// // // // // // //     console.log('2')
+// // // // // // // }
+// // // // // // // else{
+// // // // // // //     console.log("3")
+// // // // // // // }
+
+// // // // // // a=prompt(parseInt("Enter the number: "))
+// // // // // // b=prompt(parseInt("Enter the number: "))
+// // // // // // c=prompt(parseInt("Enter the number: "))
+// // // // // // d=prompt(parseInt("Enter the number: "))
+// // // // // // if (a==b){
+// // // // // //     console.log("hello")
 // // // // // // }
-// // // // // // else {
-// // // // // //     console.log("you can't vote!")
-// // // // // // }
-
-
-// // // // // // let mode = prompt("Enter the color: ")
-// // // // // // let color;
-
-// // // // // // if(mode  === 'dark'){
-// // // // // //     color = "black";
-// // // // // // }
-// // // // // // if (mode === 'light'){
-// // // // // //     color="white";
-// // // // // // }
-// // // // // // console.log(color);
-
-
-// // // // // // ##conditional senten ce to print number even or odd
-// // // // // // let a=parseInt(prompt("Enter the number : "));
-// // // // // // let number;
-// // // // // // if (a % 2 == 0){
-// // // // // //     number = "Even";
+// // // // // // else if (c==d){
+// // // // // //     console.log("hello")
 // // // // // // }
 // // // // // // else{
-// // // // // //   number = "odd";
+// // // // // //     console.log("nonononono")
 // // // // // // }
-// // // // // // console.log(number)
-
-// // // // // // ####qstn3
-// // // // // // let mode=prompt("Enter the color: ");
-// // // // // // let color;
-// // // // // // if(mode === "dark"){
-// // // // // //     color="black";
-// // // // // // }
-// // // // // // else if (mode === "blue")
-// // // // // // {
-// // // // // //     color="blue";
-// // // // // // }
-// // // // // // else if(mode === "pink")
-// // // // // // {
-// // // // // //     color="pink";
-// // // // // // }
-// // // // // // else{
-// // // // // //     color="white";
-// // // // // // }
-// // // // // // console.log(color);
-
-
-// // // // // /////
-// // // // // let a=parseInt(prompt("enter the number: "));
-// // // // // let number;
-// // // // // if (a  >= 80){
-// // // // //     number = "It is A";
+// // // // // a=prompt(parseInt("Enter the number: "))
+// // // // // if (a>0){
+// // // // //     console.log("It's POsitive")
 // // // // // }
-// // // // // else if (a >= 70){
-// // // // //   number = "It is B";
-// // // // // }
-// // // // // else if (a >= 60){
-// // // // //     number = "It is C";
-// // // // // }
-// // // // // else if (a >= 50){
-// // // // //     number = "It is D";
+// // // // // else if (a<0){
+// // // // //     console.log("It's Negative")
 // // // // // }
 // // // // // else{
-// // // // //     number="it is F";
+// // // // //     console.log("NaN")
 // // // // // }
 
-// // // // // console.log(number)
 
-
-// // // // let a=prompt(parseInt("enter the number: "));
-// // // // let b=prompt(parseInt("enter the second number: "));
-// // // // if (a==b){
-// // // //     console.log("1")
+// // // // a=prompt(parseInt("Enter the number: "))
+// // // // if (a%2==0){
+// // // //     console.log("its even number ")
 // // // // }
-// // // // else if (a>b){
-// // // //     console.log('2')
-// // // // }
-// // // // else{
-// // // //     console.log("3")
+// // // // else if(a%2 !=0){
+// // // //     console.log("It's odd number")
 // // // // }
 
-// // // a=prompt(parseInt("Enter the number: "))
-// // // b=prompt(parseInt("Enter the number: "))
-// // // c=prompt(parseInt("Enter the number: "))
-// // // d=prompt(parseInt("Enter the number: "))
-// // // if (a==b){
-// // //     console.log("hello")
+
+// // // // let name='bibek';
+// // // // for (i=0; i > name.length ;i++){
+// // // //     console.log(name[i])
+// // // // }
+
+// // // let stdName = ["Bibek"];
+// // // for (let i = 0; i < stdName.length; i++) {
+// // //     console.log(stdName[i]);
 // // // }
-// // // else if (c==d){
-// // //     console.log("hello")
-// // // }
-// // // else{
-// // //     console.log("nonononono")
-// // // }
-// // a=prompt(parseInt("Enter the number: "))
-// // if (a>0){
-// //     console.log("It's POsitive")
-// // }
-// // else if (a<0){
-// //     console.log("It's Negative")
-// // }
-// // else{
-// //     console.log("NaN")
-// // }
 
 
-// a=prompt(parseInt("Enter the number: "))
-// if (a%2==0){
-//     console.log("its even number ")
-// }
-// else if(a%2 !=0){
-//     console.log("It's odd number")
+// // let z=prompt(parseInt("enter the number: "))
+// // let factorization=1;
+// // for(let i = 1 ;i<=z ;i++){
+// //     factorization=i*factorization
+// //     console.log("your entered number i.e",z,"and your total value is",factorization)
+// // }
+// let numTerms = parseInt(prompt("Enter the number of terms: "));
+// let first = 0;
+// let second = 1;
+// console.log(first);
+// console.log(second);
+// for (let i=2;i<numTerms;i++){
+//     next=first+second;
+//     console.log(next);
+//     first=second;
+//     second=next;
 // }
