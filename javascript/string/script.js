0bfaef0d39618d9e0686b503229bba49e2e99da4
Bibek Pandey <145880788+imBibek-Pandey@users.noreply.github.com>
2024-03-20 16:25:11 +0545

    string

--- javascript/string/script.js
+++ javascript/string/script.js
@@ -1,18 +1,40 @@
-// // // // let a="bibekpandye"
-// // // // console.log(a.indexOf("bk"))
+// // // // // // // // let a="bibekpandye"
+// // // // // // // // console.log(a.indexOf("bk"))
 
-// // // let a="biebk"
-// // // strArr=Array.from(a);
-// // // console.log(strArr);
+// // // // // // // let a="biebk"
+// // // // // // // strArr=Array.from(a);
+// // // // // // // console.log(strArr);
 
-// // let fruits=['apple','oranges','banana',['syaau','kera']]
-// // let apple=fruits.flat(Infinity)
-// // console.log(apple)
-
-
-// let aNumber=[1,2,3,4,5,6,7,7,8]
-// console.log(math.random(aNumber))
+// // // // // // let fruits=['apple','oranges','banana',['syaau','kera']]
+// // // // // // let apple=fruits.flat(Infinity)
+// // // // // // console.log(apple)
 
 
-let a="beibek"
-console.log(a.length);
+// // // // // let aNumber=[1,2,3,4,5,6,7,7,8]
+// // // // // console.log(math.random(aNumber))
+
+
+// // // let a=prompt("Enter the character: ");
+// // // console.log(a.length);
+
+// // let Fruits=["ornage","apple","banana",["beibk","pandey"]]
+// // console.log(Fruits)
+// // let trees=Fruits.flat(Infinity)
+// // console.log(trees)
+
+// // let MyArray=[1,2,3,4,5,6,7]
+// // for(let i=0;i <MyArray.length;i++){
+// //     console.log(MyArray[i]);
+// // }
+
+
+// let names = ["biebek"];
+
+// for (let i = 0; i < names.length; i++) {
+//     console.log(names[i]);
+// }
+
+const numbers = [1, 2, 3, 4, 5, 6];
+
+const evenNumbers = numbers.filter(num => num % 2 === 0);
+console.log(evenNumbers);
