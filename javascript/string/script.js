f657f782752ceb991d80891cf130934d11de8bcf
Bibek Pandey <145880788+imBibek-Pandey@users.noreply.github.com>
2024-03-20 12:34:44 +0545

    string

---
+++ javascript/string/script.js
@@ -0,0 +1,18 @@
+// // // // let a="bibekpandye"
+// // // // console.log(a.indexOf("bk"))
+
+// // // let a="biebk"
+// // // strArr=Array.from(a);
+// // // console.log(strArr);
+
+// // let fruits=['apple','oranges','banana',['syaau','kera']]
+// // let apple=fruits.flat(Infinity)
+// // console.log(apple)
+
+
+// let aNumber=[1,2,3,4,5,6,7,7,8]
+// console.log(math.random(aNumber))
+
+
+let a="beibek"
+console.log(a.length);
