4d7aeacfd34c5609d6aa050ccb89414ea232a3ab
Bibek Pandey <145880788+imBibek-Pandey@users.noreply.github.com>
2024-03-24 23:01:32 +0545

    creating new element

--- javascript/Dom edit and remove/scripts.js
+++ javascript/Dom edit and remove/scripts.js
@@ -1,10 +1,20 @@
-const h2all = document.querySelectorAll('h2');
-console.log(h2all);
-h2all[0].innerHTML;
-h2all.forEach(function(h){
-    h.style.color="red";
-    h.style.backgroundColor="yellow";
-    h.style.padding="10px";
-    h.style.border="1px solid black";  
-    h.style.textAlign="center";
-});
+//##########editing and playing with differet selector inside the dom
+//  const h2all = document.querySelectorAll('h2');
+// console.log(h2all);
+// h2all[0].innerHTML;
+// h2all.forEach(function(h){
+//     h.style.color="red";
+//     h.style.backgroundColor="yellow";
+//     h.style.padding="10px";
+//     h.style.border="1px solid black";  
+//     h.style.textAlign="center";
+// });
+
+function addlanguage(langname){
+    const p =document.createElement('p');
+    p.appendChild(document.createTextNode(langname));
+}
+
+addlanguage('nepali');
+addlanguage('engllish');
+addlanguage('hindi');
