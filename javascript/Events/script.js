5022bafa7cc518786738da4c9d4dfc27b215d319
Bibek Pandey <145880788+imBibek-Pandey@users.noreply.github.com>
2024-03-26 23:12:33 +0545

    eventlistner concept

---
+++ javascript/Events/script.js
@@ -0,0 +1,51 @@
+// const one = document.getElementById('owl');
+//         one.addEventListener('clicked', function(e) {
+//             alert("you hvae clicked wrong picture it is not from Butwal")
+//             console.log("owl clicked")
+//             console.log(e.timeStamp);
+//             console.log(e.defaultPrevented)
+//             console.log(e.target)
+//             console.log(e.screenX)
+//             console.log(e.screenX)
+//             console.log(e.shiftKey)
+//             console.log(e.ctrlKey)
+//             console.log(e.altKey)
+//         },true)
+
+//         const two= document.getElementById('images');
+//         two.addEventListener('click', function(e) {
+//             alert("you hvae clicked wrong picture it is not from Butwal")
+//             console.log("images clicked")
+//             console.log(e.timeStamp);
+//             console.log(e.defaultPrevented)
+//             console.log(e.target)
+//             console.log(e.screenX)
+//             console.log(e.screenX)
+//             console.log(e.shiftKey)
+//             console.log(e.ctrlKey)
+//             console.log(e.altKey)
+//         },true)
+// <!-- #####bubling 
+// ==inside to outside
+// ##capturing
+
+// ===outside to inside -->
+//         const Images = document.querySelector('#images')
+//         Images.addEventListener("click",function(e){
+//             console.log("hi")
+//             console.log(e.target.tagName);
+//             if(e.target.Tagname === "IMG"){
+//                 console.log(e.target.id);
+//                 const removeChild=e.target.parentNode
+//                 removeChild.remove()
+
+//             }
+
+//         })
+        
+//  const owl = document.getElementById('owl')
+//   owl.addEventListener('click',function(e){
+//   alert("hello bibek")
+//  })
+
+       
