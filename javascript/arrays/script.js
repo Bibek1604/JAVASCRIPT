9f4a7d808c1a33e031ab8b59f898fb9f417bcb2b
Bibek Pandey <145880788+imBibek-Pandey@users.noreply.github.com>
2024-04-01 18:46:53 +0545

    certain

--- javascript/arrays/script.js
+++ javascript/arrays/script.js
@@ -1,67 +1,67 @@
-// // // // // // // // // ++++ACCESSING THE ARRAY
+// ++++ACCESSING THE ARRAY
 
-// // // // // // // // // // 0
-// // // // // // // // // let Fruits =["apple","banana","orange"];
-// // // // // // // // // console.log(Fruits[0]);
-// // // // // // // // // console.log(Fruits[1]);
-// // // // // // // // // console.log(Fruits[2]);
-// // // // // // // // // console.log(Fruits[-1]);
-// // // // // // // // // console.log(Fruits[-2]);
-// // // // // // // // // console.log(Fruits[-3]);44
-
-
-
-
-// // // // // // // // // ++++MOdifyting the array
-// // // // // // // // // let Fruits =["apple","banana","orange"];
-// // // // // // // // // Fruits[2]=1;
-// // // // // // // // // console.log(Fruits)
-// // // // // // // // let Fruits =["apple","banana","orange"];
-// // // // // // // // Fruits[1]="apple"
-// // // // // // // // console.log(Fruits)
-// // // // // // // // Fruits[1]="banana"
-// // // // // // // // console.log(Fruits)
+// // 0
+// let Fruits =["apple","banana","orange"];
+// console.log(Fruits[0]);
+// console.log(Fruits[1]);
+// console.log(Fruits[2]);
+// console.log(Fruits[-1]);
+// console.log(Fruits[-2]);
+// console.log(Fruits[-3]);44
 
 
 
-// // // // // // // ////Array traversal or iterative over array
-// // // // // // // let Fruits =["apple","banana","orange"];
-// // // // // // // for (let item of Fruits){
-// // // // // // //     console.log(item)
-// // // // // // // for (let item in Fruits){
-// // // // // // //     console.log(item)
-// // // // // // // }
-// // // // // // // }
-// // // // // // // // for (let item=0; item<Fruits.length;item++){
-// // // // // // // //     console.log(Fruits[item]);
-// // // // // // // // }
 
-// // // // // // let Fruits =["apple","banana","orange"];
-// // // // // // Fruits.forEach((curElem, index, arr) => {
-// // // // // //     console.log(arr);
-// // // // // // });
+// ++++MOdifyting the array
+// let Fruits =["apple","banana","orange"];
+// Fruits[2]=1;
+// console.log(Fruits)
+// let Fruits =["apple","banana","orange"];
+// Fruits[1]="apple"
+// console.log(Fruits)
+// Fruits[1]="banana"
+// console.log(Fruits)
 
 
-// // // // // let myArray=[1,2,3,4]
-// // // // // console.log(myArray[2])
 
-// // // // const marvel=["thor","ironman","spiderman"]
-// // // // const dc_heros=["superman",'flash','actor']
-// // // // marvel.push(dc_heros)
-// // // // console.log(marvel)
-// // // // console.log(marvel[2][3])
-// // // const new_array = [1, 2, 3, [4, 5, 6], [7, 8, 9]];
-// // // const old_array = new_array.flat(Infinity);
-// // // console.log(old_array);
+// Array traversal or iterative over array
+// let Fruits =["apple","banana","orange"];
+// for (let item of Fruits){
+//     console.log(item)
+// for (let item in Fruits){
+//     console.log(item)
+// }
+// }
+// for (let item=0; item<Fruits.length;item++){
+//     console.log(Fruits[item]);
+// }
 
-// // const newLoop=['apple','banana',["kera",'syau'],"orange","mango",["suntala","aap"]]
-// // const oldLoop=newLoop.flat(Infinity)
-// // console.log(oldLoop)
+// let Fruits =["apple","banana","orange"];
+// Fruits.forEach((curElem, index, arr) => {
+//     console.log(arr);
+// });
 
 
-// // console.log(Array.isArray("bibek"))
+// let myArray=[1,2,3,4]
+// console.log(myArray[2])
 
-// // console.log(Array.from("Bibek"))
+// const marvel=["thor","ironman","spiderman"]
+// const dc_heros=["superman",'flash','actor']
+// marvel.push(dc_heros)
+// console.log(marvel)
+// console.log(marvel[2][3])
+// const new_array = [1, 2, 3, [4, 5, 6], [7, 8, 9]];
+// const old_array = new_array.flat(Infinity);
+// console.log(old_array);
+
+// const newLoop=['apple','banana',["kera",'syau'],"orange","mango",["suntala","aap"]]
+// const oldLoop=newLoop.flat(Infinity)
+// console.log(oldLoop)
+
+
+// console.log(Array.isArray("bibek"))
+
+// console.log(Array.from("Bibek"))
 
 
 // const MyName=["bibek","pandey","is hero"];
